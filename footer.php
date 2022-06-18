feb9fd5d47ed115de7c7094cad0bed471ee14561
Pang Jia Mei <josiejmpang@gmail.com>
2022-06-18 22:02:58 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>footer page</title>
+    </head>
+
+    <body>
+        <hr/>
+        <h3 style="text-align:right; font-size:small">
+        <font color='black' >&copy; Copyright 2022, PCISS System</font></h3> 
+    </body>
+</html>
