7e7fe9415dd9a6b56ebf63f91c366de8cf16384e
Pang Jia Mei <josiejmpang@gmail.com>
2022-06-18 22:38:33 +0800

    Update and rename lecturer.html to lecturer.php

--- lecturer.php
+++ lecturer.php
@@ -10,33 +10,7 @@
 </head>
 
 <body>
-    <div class="side-menu">
-        <div class="brand-name">
-            <h1>PCISS</h1>
-        </div>
-        <ul>
-            <li><img src="image/reading-book (1).png" alt="">&nbsp;<span>Students</span> </li>
-            <li><img src="image/teacher2.png" alt="">&nbsp;<span>Student Schedule</span> </li>
-            <li><img src="image/school.png" alt="">&nbsp;<span>Assessment</span> </li>
-            <li><img src="image/settings.png" alt="">&nbsp;<span>Settings</span> </li>
-        </ul>
-    </div>
-    <div class="container">
-        <div class="header">
-            <div class="nav">
-                <div class="search">
-                    <input type="text" placeholder="Search..">
-                    <button type="submit"><img src="image/search.png" alt=""></button>
-                </div>
-                <div class="user">
-                    <a href="#" class="btn">Add New</a>
-                    <img src="image/notifications.png" alt="">
-                    <div class="img-case">
-                        <img src="image/user.png" alt="">
-                    </div>
-                </div>
-            </div>
-        </div>
+    <?php include("lechead.php"); ?>
         <div class="content">
             <div class="cards">
                 <div class="card">
@@ -139,7 +113,9 @@
                     </table>
                 </div>
             </div>
-            <footer>&copy; Copyright 2022, PCISS System</footer> 
+            <?php 
+            include('footer.php');
+            ?>
         </div>
     </div>
 </body>
