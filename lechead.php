f42b7d1a78ee784d72db1befd23a2cc5441ef2c9
Pang Jia Mei <josiejmpang@gmail.com>
2022-06-18 22:36:27 +0800

    Add files via upload

---
+++ lechead.php
@@ -0,0 +1,37 @@
+<html>
+
+<head>
+    <title>header page</title>
+</head>
+
+<body>
+<div class="side-menu">
+        <div class="brand-name">
+            <h1>PCISS</h1>
+        </div>
+        <ul>
+            <li><img src="image/reading-book (1).png" alt="">&nbsp;<span>Students</span> </li>
+            <li><img src="image/teacher2.png" alt="">&nbsp;<span>Student Schedule</span> </li>
+            <li><img src="image/school.png" alt="">&nbsp;<span>Assessment</span> </li>
+            <li><img src="image/settings.png" alt="">&nbsp;<span>Settings</span> </li>
+        </ul>
+    </div>
+    <div class="container">
+        <div class="header">
+            <div class="nav">
+                <div class="search">
+                    <input type="text" placeholder="Search..">
+                    <button type="submit"><img src="image/search.png" alt=""></button>
+                </div>
+                <div class="user">
+                    <a href="#" class="btn">Add New</a>
+                    <img src="image/notifications.png" alt="">
+                    <div class="img-case">
+                        <img src="image/user.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+</body>
+
+</html>
