e422103596a0bcffee80aeab49e7240c8b508b13
Nur Afiqah Raman <80899173+NurAfiqahRaman@users.noreply.github.com>
2022-06-21 11:22:52 +0800

    Update and rename coviddetails.html to coviddetails.php

--- coviddetails.php
+++ coviddetails.php
@@ -15,9 +15,9 @@
             <h1>PCISS</h1>
         </div>
         <ul>
-            <li><a href ="studentprofile.html" target="_self" ><img src="image/reading-book (1).png" alt="">&nbsp;<span>Students</span></a> </li>
+            <li><a href ="studentprofile.php" target="_self" ><img src="image/reading-book (1).png" alt="">&nbsp;<span>Students</span></a> </li>
             <li><a href ="studentschedule.html" target="_self" ><img src="image/teacher2.png" alt="">&nbsp;<span>Student Schedule</span></a> </li>
-            <li><a href ="studentassessment.html" target="_self" ><img src="image/school.png" alt="">&nbsp;<span>Assessment</span></a> </li>
+            <li><a href ="studentassessment.php" target="_self" ><img src="image/school.png" alt="">&nbsp;<span>Assessment</span></a> </li>
             <li><a href ="settings.html" target="_self" ><img src="image/settings.png" alt="">&nbsp;<span>Settings</span></a> </li>
         </ul>
     </div>
@@ -50,35 +50,49 @@
 
                        </thead>
 
-                       <tbody>
-                        <tr>
-                            <th>Matric Number</th>
-                            <td>B031910000</td>
-                        </tr>
-                        <tr>
-                            <th>My Sejahtera ID</th>
-                            <td>60115672819</td>
-                        </tr>
-                        <tr>
-                            <th>Covid-19 Start Date</th>
-                            <td>1 January 2022</td>
-                        </tr>
-                        <tr>
-                            <th>Symptoms</th>
-                            <td>Cough</td>
-                        </tr>
-                        <tr>
-                            <th>Covid-19 last date (prediction)</th>
-                            <td>8 January 2022</td>
-                        </tr>
+                       <?php
+                        include 'conn.php';
+
+                        $matric_number=$_REQUEST['matric_number'];
+                        $sql = "SELECT * FROM symptoms WHERE matric_number='$matric_number'";
+                        $result=$conn->query($sql);
+                        $result = $conn->query($sql) or die($conn->error);
                         
-                       
-                                
-                                <td>
-                                   
-                                    <a class='btn btn-danger btn-sm' href="studentprofile.html" >Back</a>    
-                                </td>
+
+                        while($row = $result->fetch_assoc()) 
+                        {
+                            //$student_id=$row['student_id'];
+                        ?>
                             
+                            <tr>
+                            <th>Matric Number</th>
+                            <td><?php echo $row['matric_number'] ?></td>
+                        </tr>
+                        <tr>
+                            <th>MySejahtera ID</th>
+                            <td><?php echo $row['mysejahtera_id'] ?></td>
+                        </tr>
+                        <tr>
+                            <th>Symptoms Details</th>
+                            <td><?php echo $row['symptoms_details'] ?></td>
+                        </tr>
+                        <tr>
+                            <th>Symptoms Condition</th>
+                            <td><?php echo $row['symptoms_condition'] ?></td>
+                        </tr>
+                        <tr>
+                            <th>Symptoms Start Date</th>
+                            <td><?php echo $row['symptoms_start_date'] ?></td>
+                        </tr>
+                        <tr>
+                            <th>Symptoms Last Date</th>
+                            <td><?php echo $row['symptoms_last_date'] ?></td>
+                        </tr>
+                           
+                            
+                        <?php
+                        }
+                        ?>
                        </tbody>
                    </table>
                 </div>
